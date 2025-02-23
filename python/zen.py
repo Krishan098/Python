724cc4ea74972302f70c6bacab6aa226d9eec92e
krim798 <168003081+Krishan098@users.noreply.github.com>
2025-02-23 11:22:11 +0530

    changes

---
+++ python/zen.py
@@ -0,0 +1,45 @@
+import this
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+# object. hit tab twice- all opertions
+# \r: takes back to start of line
+# _ takes the last output and performs operations
+#help(...): shows what can be done
+#''' ''' triple quotes used for escaping
+#.replace() 
+''' Formatting:
+            my_age=40
+            f'My age is {my_age}'
+'''
+'''
+    :. Uppercase letters are smaller than lowercase letters
+    :. digits are smaller than letters.
+'''
+
+'''
+        Iterators: object that can return its members one at a time.
+        Objects: 
+'''
+
+# Parameters and arguments
+# A Python function can have parameters. The values we pass through these parameters are called arguments.
